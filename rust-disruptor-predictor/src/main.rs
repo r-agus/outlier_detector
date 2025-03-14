6a5460dca2acf5f65e6dea98be21920725e7b13e
Ruben Agustin <ragustin@sire.es>
2025-03-14 13:50:11 +0100

    chore: commented a new example

--- rust-disruptor-predictor/src/main.rs
+++ rust-disruptor-predictor/src/main.rs
@@ -2,28 +2,33 @@
 
 mod sets;
 
-use sets::{Set, SetCollection, Xs};
+use sets::{Set, SetCollection, Taxonomy, Xs};
 
 fn main() {
     let mut set_collection = SetCollection::new();
     
     let y1 = Set::new("y1".to_string(), vec![5.0, 6.0, 7.0, 8.0]);
     let y2 = Set::new("y2".to_string(), vec![14.0, 15.0, 16.0, 17.0]);
+    // let y3 = Set::new("y3".to_string(), vec![2.0, 24.0, 25.0]);
+    // let y4 = Set::new("y4".to_string(), vec![13.0, 13.0, 13.0, 13.0, 13.0]);
 
     set_collection.add_set(y1);
     set_collection.add_set(y2);
+    // set_collection.add_set(y3);
+    // set_collection.add_set(y4);
 
     let xs = Xs {value: 13.0};
 
     let centroids_matrix = set_collection.calculate_centroids_matrix(&xs);
 
-    println!("La matriz de centroides es: {:?}", centroids_matrix);
+    println!("La matriz de centroides es: {}", centroids_matrix.to_string(3));
 
     let tx = set_collection.taxonomize(xs);
-    println!("{:?}", tx);
+
+    println!("{}", Taxonomy::vec_to_string(&tx));
 
     let prob = set_collection.pobabilize(&tx);
-    println!("La matriz de probabilidades es: {:?}", prob);
+    println!("\nLa matriz de probabilidades es: {}", prob.to_string(3));
 
     let prob_avg = set_collection.probabilize_avg(&tx);
     println!("La probabilidad media es: {:?}", prob_avg);
